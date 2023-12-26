62522e0d125df8fee703aba60732a1f848ba9adb
Manjunath.Hosamani <manjunathhosamani67@gmail.com>
2023-12-26 23:34:06 +0530

    Made changes and calculation

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const inputs = document.querySelectorAll("input");
 const btn = document.querySelector("button");
 const errMeassage = document.querySelectorAll(".errMeassage");
-const ageYear = document.querySelectorAll(".Years");
-const ageMonth = document.querySelectorAll(".Months");
-const ageDay = document.querySelectorAll(".Days");
+const ageYear = document.querySelector(".Years");
+const ageMonth = document.querySelector(".Months");
+const ageDay = document.querySelector(".Days");
 
 
 const date = new Date();
@@ -11,36 +11,52 @@ let currentDay = date.getDate();
 let currentMonth = date.getMonth();
 let currentYear = date.getFullYear();
 
-const bday = inputs[0];
-const bmonth = inputs[1];
-const byear = inputs[2];
+let bday = inputs[0];
+let bmonth = inputs[1];
+let byear = inputs[2];
+
 
 
 function calculate(){
-    let ageInYear 
-    let ageInMonths;
-    let ageInDays;
+    let ageInYear = currentYear - byear.value;
+    let ageInMonths = (currentMonth + 1) - bmonth.value;
+    let ageInDays = currentDay - bday.value;
+    // console.log(currentMonth)
 
-    if(currentMonth >= bmonth.value && currentDay  >= bday.value){
-       ageInYear = currentYear-byear.value;
-       ageInMonths = currentMonth-bmonth.value;
+    if (ageInDays < 0) {
+        let daysInLastMonth = new Date(currentYear, currentMonth - 1, 0).getDate();
+        ageInDays = daysInLastMonth - bday.value + currentDay;
+        ageInMonths--;
     }
-    else{
+
+    // Adjust for negative months
+    if (ageInMonths < 0) {
+        ageInMonths += 12;
         ageInYear--;
-        if(currentDay < bday){
-        ageInMonths = currentMonth - bmonth.value;
-        ageInMonths = 12-ageInMonths;
-        }
-
     }
-    console.log(ageInYear +" years and months" +ageInMonths)
 
-    return ageInYear,ageInMonths;
+    // if(currentMonth >= bmonth.value && currentDay  >= bday.value){
+    //    ageInYear = currentYear-byear.value;
+    //    ageInMonths = currentMonth-bmonth.value;
+    // }
+    // else{
+    //     ageInYear--;
+    //     if(currentDay < bday){
+    //     ageInMonths = currentMonth - bmonth.value;
+    //     ageInMonths = 12-ageInMonths;
+    //     }
+
+    // }
+
+
+    // console.log(ageInYear +" years and months" +ageInMonths,ageInDays)
+
+    return { year:ageInYear,
+             month:ageInMonths,
+             day:ageInDays
+            };
 
 }
-
-
-
 
 function showError(indexText){
     errMeassage[indexText].classList.add("errMessage");
@@ -55,21 +71,30 @@ function hideError(indexText){
 
 console.log(btn);
 btn.addEventListener("click", (event)=>{
+    let age;
     event.preventDefault();
     console.log("Button is clicked");
     // console.log(currentDay-bday.value);
     // console.log(currentDay)
-    console.log(byear.value);
+    // console.log(byear.value);
     inputs.forEach((items,index)=>{
         if(items.value===""){
             showError(index)
         }
         else{
             hideError(index)
-            calculate();
+            age = calculate();
+            // console.log(age)
+            ageDay.innerHTML = age.day;
+            console.log(age.day);
+            // console.log(ageDay.value);
+            ageMonth.innerHTML = age.month;
+            ageYear.innerHTML = age.year;
+            
         }
 
     });
+    
 
 
 
